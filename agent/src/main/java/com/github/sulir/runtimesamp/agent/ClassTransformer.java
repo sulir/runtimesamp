7ec837c82c451ce075a503ef87f3f233aec33b9c
Matúš Sulír <matus.sulir@gmail.com>
2018-01-31 22:55:59 +0100

    Fix ASM common superclass computation

--- agent/src/main/java/com/github/sulir/runtimesamp/agent/ClassTransformer.java
+++ agent/src/main/java/com/github/sulir/runtimesamp/agent/ClassTransformer.java
@@ -1,24 +1,28 @@
 package com.github.sulir.runtimesamp.agent;
 
+import org.mutabilitydetector.asm.NonClassloadingClassWriter;
+import org.mutabilitydetector.asm.typehierarchy.TypeHierarchyReader;
 import org.objectweb.asm.*;
 import org.objectweb.asm.tree.MethodNode;
 
 public class ClassTransformer {
     private ClassReader reader;
+    private TypeHierarchyReader hierarchy;
 
-    public ClassTransformer(byte[] classBytes) {
+    public ClassTransformer(byte[] classBytes, TypeHierarchyReader hierarchy) {
         reader = new ClassReader(classBytes);
+        this.hierarchy = hierarchy;
     }
 
     public byte[] transform() {
-        ClassWriter writer = new ClassWriter(reader, ClassWriter.COMPUTE_FRAMES);
+        ClassWriter writer = new NonClassloadingClassWriter(reader, ClassWriter.COMPUTE_FRAMES, hierarchy);
 
-        ClassVisitor classVisitor = new ClassVisitor(Opcodes.ASM6, writer) {
+        ClassVisitor classVisitor = new ClassVisitor(Opcodes.ASM5, writer) {
             @Override
             public MethodVisitor visitMethod(int access, String name, String desc,
                                              String signature, String[] exceptions) {
                 MethodVisitor methodVisitor = super.visitMethod(access, name, desc, signature, exceptions);
-                return new MethodNode(Opcodes.ASM6, access, name, desc, signature, exceptions) {
+                return new MethodNode(Opcodes.ASM5, access, name, desc, signature, exceptions) {
                     @Override
                     public void visitEnd() {
                         if (instructions.size() != 0 && (access & Opcodes.ACC_SYNTHETIC) == 0)
--- agent/src/main/java/com/github/sulir/runtimesamp/agent/SampAgent.java
+++ agent/src/main/java/com/github/sulir/runtimesamp/agent/SampAgent.java
@@ -1,9 +1,14 @@
 package com.github.sulir.runtimesamp.agent;
 
+import org.mutabilitydetector.asm.typehierarchy.ConcurrentMapCachingTypeHierarchyReader;
+import org.mutabilitydetector.asm.typehierarchy.TypeHierarchyReader;
+
 import java.lang.instrument.Instrumentation;
 import java.util.regex.Pattern;
 
 public class SampAgent {
+    private static final TypeHierarchyReader hierarchy
+            = new ConcurrentMapCachingTypeHierarchyReader(new TypeHierarchyReader());
 
     public static void premain(String agentArgs, Instrumentation inst) {
         Pattern include = Pattern.compile((agentArgs == null) ? ".*" : agentArgs);
@@ -15,7 +20,7 @@ public class SampAgent {
             try {
                 if (className != null && include.matcher(className).matches()
                         && !exclude.matcher(className).matches()) {
-                    ClassTransformer transformer = new ClassTransformer(classfileBuffer);
+                    ClassTransformer transformer = new ClassTransformer(classfileBuffer, hierarchy);
                     return transformer.transform();
                 }
             } catch (Exception e) {
